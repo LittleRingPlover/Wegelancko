dc3b8e549fa0d157192114f04d7b8dfd20375921
albrz <al.brzostowicz@gmail.com>
2022-12-30 19:58:05 +0100

    Update urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,7 +7,6 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home/', views.home, name='home'),
     path('recipes/', views.show_recipes, name='show_recipes'),
     path('my-recipes/', views.show_my_recipes, name='show_my_recipes'),
     path('recipe/<int:pk>/', views.show_recipe, name='show_recipe'),
